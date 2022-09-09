d613e8e0ad994f658c64d44042943bf619ea0ec8
jlim0130 <jlim0130@student.monash.edu>
2022-09-10 02:49:24 +0800

    Clean up unused component

--- Projects/agileApp/Frontend/src/app/taskdetail/taskdetail.component.ts
+++ Projects/agileApp/Frontend/src/app/taskdetail/taskdetail.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, EventEmitter, Output,Inject } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { task } from '../types';
 import { DbService } from '../services/db.service';
-import { UpdatetaskComponent } from '../updatetask/updatetask.component';
 import { TaskformComponent } from '../taskform/taskform.component';
 
 @Component({
@@ -17,7 +16,6 @@ export class TaskdetailComponent implements OnInit {
   constructor(public dialogRef: MatDialogRef<TaskdetailComponent>,
     @Inject(MAT_DIALOG_DATA) public data: task,
     private db:DbService,
-    public dialogRefForm:MatDialogRef<UpdatetaskComponent>,
     private dialog:MatDialog) { }
 
   ngOnInit(): void {
@@ -30,15 +28,11 @@ export class TaskdetailComponent implements OnInit {
     })
   }
 
-
-
-
   onDelete():void{
     this.db.deleteTask(this.data).subscribe(res=>{
       console.log("Success")
     },err=>{
       console.log(err)
-      this.taskService.getData()
     })
   }
 
