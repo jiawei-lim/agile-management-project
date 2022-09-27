e2318fba3ec46b1622fee1f9fde278012221ed04
TongJetKit <jton0028@student.monash.edu>
2022-09-27 15:59:18 +0800

    add remove time log and constraints

--- Projects/agileApp/Frontend/src/app/timelog/timelog.component.ts
+++ Projects/agileApp/Frontend/src/app/timelog/timelog.component.ts
@@ -24,30 +24,53 @@ export class TimelogComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  logTime():void{
+  logTime(name:string):void{
     var index = this.data.total_time.indexOf(":")
     var h = this.data.total_time.substring(0,index)
     var m = this.data.total_time.substring(index+1,5)
+    console.log(name)
+    
 
     // console.log(h,m)
     // console.log("timelog:",this.timeDataForm.value)
     // console.log(Number(h)+(this.timeDataForm.controls['hour'].value))
     // console.log(this.data.total_time)
     
-    
-    if((Number(h)+this.timeDataForm.controls['hour'].value)<10){
-      h = '0' + String(Number(h)+this.timeDataForm.controls['hour'].value);
-    }
-    else{
-      h = String(Number(h)+this.timeDataForm.controls['hour'].value);
-    }
+    if(name==="add"){
+      if((Number(h)+this.timeDataForm.controls['hour'].value)<10){
+        h = '0' + String(Number(h)+this.timeDataForm.controls['hour'].value);
+      }
+      else{
+        h = String(Number(h)+this.timeDataForm.controls['hour'].value);
+      }
 
-    if(Number(m)+this.timeDataForm.controls['minutes'].value<10){
-      m = '0' +String(Number(m)+this.timeDataForm.controls['minutes'].value);
+      if(Number(m)+this.timeDataForm.controls['minutes'].value<10){
+        m = '0' +String(Number(m)+this.timeDataForm.controls['minutes'].value);
+      }
+      else{
+        m = String(Number(m)+this.timeDataForm.controls['minutes'].value);
+      }
     }
     else{
-      m = String(Number(m)+this.timeDataForm.controls['minutes'].value);
+      if(h<this.timeDataForm.controls['hour'].value || m<this.timeDataForm.controls['minutes'].value){
+        alert("Time removed more than allocated")
+        return
+      }
+      if((Number(h)-this.timeDataForm.controls['hour'].value)<10){
+        h = '0' + String(Number(h)-this.timeDataForm.controls['hour'].value);
+      }
+      else{
+        h = String(Number(h)-this.timeDataForm.controls['hour'].value);
+      }
+
+      if(Number(m)-this.timeDataForm.controls['minutes'].value<10){
+        m = '0' +String(Number(m)-this.timeDataForm.controls['minutes'].value);
+      }
+      else{
+        m = String(Number(m)-this.timeDataForm.controls['minutes'].value);
+      }
     }
+    
     this.data.total_time = h+":"+m
 
     this.submitClicked.emit(this.data)
