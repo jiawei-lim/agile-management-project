f1af459dbf12b306d355a86389f23e7188e02cde
Cheng_Bo_Ren <bche0071@student.monash.edu>
2022-09-28 15:41:36 +0800

    HTML and constructor for sprint form

--- Projects/agileApp/Frontend/src/app/sprint-form/sprint-form.component.ts
+++ Projects/agileApp/Frontend/src/app/sprint-form/sprint-form.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, Inject, EventEmitter } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { sprint } from '../types';
+import { DatePipe } from '@angular/common';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-sprint-form',
@@ -7,7 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SprintFormComponent implements OnInit {
 
-  constructor() { }
+  sprintDataForm: any;
+
+  constructor(
+    public dialogRef: MatDialogRef<SprintFormComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: sprint,
+    private fb: FormBuilder
+  ) { 
+    this.sprintDataForm = this.fb.group({
+      sprint_id:[null],
+      sprint_name: [''],
+      created_date: [''],
+      end_date: ['']
+    });
+  }
 
   ngOnInit(): void {
   }
