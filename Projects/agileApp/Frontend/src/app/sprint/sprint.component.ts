1905aa31e6196397c336288f3b25f806ddf79246
Cheng_Bo_Ren <bche0071@student.monash.edu>
2022-10-01 00:27:44 +0800

    Added functionality (incomplete): manually start/end sprints

--- Projects/agileApp/Frontend/src/app/sprint/sprint.component.ts
+++ Projects/agileApp/Frontend/src/app/sprint/sprint.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { range } from 'rxjs';
 import { DbService } from '../services/db.service';
 import { task } from '../types';
 
@@ -9,18 +10,48 @@ import { task } from '../types';
 })
 export class SprintComponent implements OnInit {
 
-  @Input() sprint_id:number;
-  sprint_list:task[] = [];
+  @Input() sprint_id!: number;
+  sprint_list: task[] = [];
 
-  constructor(private db:DbService) {
-    
+  constructor(private db: DbService) {
+
   }
 
   ngOnInit(): void {
-    this.db.getTasks(this.sprint_id).subscribe((res)=>{
+    this.db.getTasks(this.sprint_id).subscribe((res) => {
       this.sprint_list = res;
-    },(err)=>{
+    }, (err) => {
       console.log(err)
+    })
+  }
+
+  // manually start sprint
+  onStart(): void {
+    this.db.getSprints().subscribe((res) => {
+      // look for corresponding sprint ID
+      for (let i = 0; i < res.length; i++) {
+        if (res[i].sprint_id == this.sprint_id) {
+          res[i].sprint_status = "active";
+          console.log(res[i])
+          // update in DB
+          this.db.updateSprintStatus(res[i]);
+        }
+      }
+    })
+  }
+
+  // manually end sprint
+  onEnd(): void {
+    this.db.getSprints().subscribe((res) => {
+      // look for corresponding sprint ID
+      for (let i = 0; i < res.length; i++) {
+        if (res[i].sprint_id == this.sprint_id) {
+          res[i].sprint_status = "ended";
+          console.log(res[i])
+          // update in DB
+          this.db.updateSprintStatus(res[i]);
+        }
+      }
     })
   }
 
